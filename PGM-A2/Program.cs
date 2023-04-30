493f86d2fdaa528283876e4a810c12c35ca4a5a8
FuzzyPickles92 <97342270+FuzzyPickles92@users.noreply.github.com>
2023-04-29 20:58:47 -0500

    Updated project.

--- PGM-A2/Program.cs
+++ PGM-A2/Program.cs
@@ -5,8 +5,6 @@
 // Professor Peggy Peeples
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace HuntingApp
 {
@@ -14,115 +12,206 @@ namespace HuntingApp
     {
         static void Main(string[] args)
         {
-            // Define dictionaries for hunter names, hunter types, and hunting rates.
-            Dictionary<int, string> HunterNames = new Dictionary<int, string>
+            // Variables
+            int numberOfDays, numberOfHunters = 0, numberOfHuntersn2 = 0;
+            float huntCost = 0.00f, huntRate = 0.00f, totalHuntRate = 0.00f, totalHuntRaten2 = 0.00f, avgHuntRate = 0.00f, avgHuntRaten2 = 0.00f;
+            string hunterName = "", hunterTypeName = "", huntCode = "", shift, hunterID, fieldCode, huntType;
+            string answer;
+
+            // Start of program
+            Console.WriteLine("Do you have a hunter to enter? yes or no");
+            answer = Console.ReadLine();
+
+            // Asks for user input
+            while (answer == "yes")
             {
-                { 100, "Hunter 1" },
-                { 200, "Hunter 2" }
-            };
+                // Shift
+                Console.WriteLine("Enter shift Code: Day or Night");
+                shift = Console.ReadLine();
 
-            Dictionary<int, string> HunterTypeNames = new Dictionary<int, string>
-            {
-                { 1, "Bow" },
-                { 2, "Gun" },
-                { 3, "Arrow" },
-                { 4, "SL-Shot" }
-            };
+                // HunterID
+                Console.WriteLine("Enter 100 or 200 for the HunterID");
+                hunterID = Console.ReadLine();
 
-            Dictionary<int, double> HuntingRates = new Dictionary<int, double>
-            {
-                { 41, 25.12 }, // Updated hunt rate for Day shift
-                { 51, 6.55 }   // Updated hunt rate for Night shift
-            };
-
-            Console.WriteLine("Welcome to the Hunting App!");
-
-            // Variables for loop control and summary calculations.
-            bool processAnotherHunter = true;
-            List<double> huntCosts = new List<double>();
-            List<double> dayGunHuntCosts = new List<double>();
-            List<double> otherHuntRates = new List<double>();
-
-            // Main loop for processing hunters.
-            while (processAnotherHunter)
-            {
-                // Declare variables for user input.
-                int shift, hunterId, fieldCode, huntType, numOfDays;
-                string hunterName, hunterTypeName;
-                double huntingRate, huntCost;
-
-                Console.WriteLine("Please enter the following details:");
-
-                // Get user input for shift code, hunter ID, field code, hunt type, and number of days to hunt.
-                do
+                if (hunterID == "100")
                 {
-                    Console.Write("Shift code (1: Day, 2: Night): ");
-                    int.TryParse(Console.ReadLine(), out shift);
-                } while (shift != 1 && shift != 2);
-
-                do
-                {
-                    Console.Write("Hunter ID (100 or 200): ");
-                    int.TryParse(Console.ReadLine(), out hunterId);
-                } while (hunterId != 100 && hunterId != 200);
-
-                do
-                {
-                    Console.Write("Field code (41 or 51): ");
-                    int.TryParse(Console.ReadLine(), out fieldCode);
-                } while (fieldCode != 41 && fieldCode != 51);
-
-                do
-                {
-                    Console.Write("Hunt type (1: Bow, 2: Gun, 3: Arrow, 4: SL-Shot): ");
-                    int.TryParse(Console.ReadLine(), out huntType);
-                } while (huntType < 1 || huntType > 4);
-
-                do
-                {
-                    Console.Write("Number of days to hunt (1-5): ");
-                    int.TryParse(Console.ReadLine(), out numOfDays);
-                } while (numOfDays < 1 || numOfDays > 5);
-
-                // Retrieve hunter and hunt type information, calculate hunting costs.
-                hunterName = HunterNames[hunterId];
-                hunterTypeName = HunterTypeNames[huntType];
-                huntingRate = HuntingRates[fieldCode];
-                huntCost = huntingRate * numOfDays;
-
-                // Display hunt details and cost for the current hunter.
-                Console.WriteLine($"\nHunter {hunterName} (ID: {hunterId}) hunted using {hunterTypeName} for {numOfDays} day(s).");
-                Console.WriteLine($"Total hunt cost: ${huntCost:0.00}");
-
-                // Update summary lists
-                huntCosts.Add(huntCost);
-                if (shift == 1 && huntType == 2)
-                {
-                    dayGunHuntCosts.Add(huntCost);
+                    hunterName = "Bubba";
                 }
-                if (shift != 1 && numOfDays >= 2)
+                else if (hunterID == "200")
                 {
-                    otherHuntRates.Add(huntingRate);
+                    hunterName = "Sammy";
+                }
+                else
+                {
+                    hunterName = "Unknown";
                 }
 
-                Console.Write("\nDo you want to process another hunter? (y/n): ");
-                processAnotherHunter = Console.ReadLine().ToLower() == "y";
+                // FieldCode
+                Console.WriteLine("Enter the field code, 41 or 51");
+                fieldCode = Console.ReadLine();
+
+                // HuntType
+                Console.WriteLine("Enter the HuntType: 1, 2, 3, or 4");
+                huntType = Console.ReadLine();
+
+                if (huntType == "1")
+                {
+                    hunterTypeName = "Bow";
+                }
+                else if (huntType == "2")
+                {
+                    hunterTypeName = "Gun";
+                }
+                else if (huntType == "3")
+                {
+                    hunterTypeName = "Arrow";
+                }
+                else if (huntType == "4")
+                {
+                    hunterTypeName = "Sl-Shot";
+                }
+                else
+                {
+                    hunterTypeName = "Unknown";
+                }
+
+                // NumberOfDays
+                Console.WriteLine("Enter the Number of days");
+                if (!int.TryParse(Console.ReadLine(), out numberOfDays))
+                {
+                    numberOfDays = 0;
+                }
+
+                // Detailed Calculations for huntRate and huntCode
+                if (fieldCode == "41")
+                {
+                    if (huntType == "1")
+                    {
+                        if (shift == "Day")
+                        {
+                            huntRate = 25.12f;
+                            huntCode = "A-B-N";
+                        }
+                        else if (shift == "Night")
+                        {
+                            huntRate = 26.12f;
+                            huntCode = "A-B-D";
+                        }
+                        else
+                        {
+                            huntRate = 126.12f;
+                            huntCode = "A-B-O";
+                        }
+                    }
+                    else if (huntType == "2")
+                    {
+                        if (shift == "Day")
+                        {
+                            huntRate = 13.22f;
+                            huntCode = "A-G-D";
+                        }
+                        else if (shift == "Night")
+                        {
+                            huntRate = 14.22f;
+                            huntCode = "A-G-N";
+                        }
+                        else
+                        {
+                            huntRate = 114.22f;
+                            huntCode = "A-G-O";
+                        }
+                    }
+                    else
+                    {
+                        huntRate = 125.12f;
+                        huntCode = "A-O";
+                    }
+                }
+                else if (fieldCode == "51")
+                {
+                    if (huntType == "3")
+                    {
+                        if (shift == "Day")
+                        {
+                            huntRate = 12.22f;
+                            huntCode = "B-A-D";
+                        }
+                        else if (shift == "Night")
+                        {
+                            huntRate = 13.22f;
+                            huntCode = "B-A-N";
+                        }
+                        else
+                        {
+                            huntRate = 113.22f;
+                            huntCode = "B-A-O";
+                        }
+                    }
+                    else if (huntType == "4")
+                    {
+                        if (shift == "Day")
+                        {
+                            huntRate = 5.55f;
+                            huntCode = "B-S-D";
+                        }
+                        else if (shift == "Night")
+                        {
+                            huntRate = 6.55f;
+                            huntCode = "B-S-N";
+                        }
+                        else
+                        {
+                            huntRate = 106.55f;
+                            huntCode = "B-S-O";
+                        }
+                    }
+                    else
+                    {
+                        huntRate = 200f;
+                        huntCode = "B-O";
+                    }
+                }
+                else
+                {
+                    huntRate = 0f;
+                    huntCode = "Unknown";
+                }
+
+                // HuntCost
+                huntCost = numberOfDays * huntRate;
+
+                // Detailed output
+                Console.WriteLine("Your hunter name is " + hunterName + ". ");
+                Console.WriteLine("Your hunter code is " + huntCode + ". ");
+                Console.WriteLine("Your hunter type name is " + hunterTypeName + ". ");
+                Console.WriteLine("The number of days you are going to hunt is " + numberOfDays + ". ");
+                Console.WriteLine("The cost is " + huntCost + ". ");
+
+                // Summary calculations
+                numberOfHunters = numberOfHunters + 1;
+                totalHuntRate = totalHuntRate + huntRate;
+                avgHuntRate = totalHuntRate / numberOfHunters;
+
+                if (shift != "Day" && numberOfDays >= 2)
+                {
+                    numberOfHuntersn2 = numberOfHuntersn2 + 1;
+                    totalHuntRaten2 = totalHuntRaten2 + huntRate;
+                    avgHuntRaten2 = totalHuntRaten2 / numberOfHuntersn2;
+                }
+
+                // Asks if loop again
+                Console.WriteLine("Do you have another to enter? YES or NO");
+                answer = Console.ReadLine();
             }
 
-            Console.WriteLine("\nSummary:");
-            Console.WriteLine($"Total hunters processed: {huntCosts.Count}");
-            Console.WriteLine($"Total hunt cost: ${huntCosts.Sum():0.00}");
-
-            string avgDayGunHunts = dayGunHuntCosts.Count > 0 ? $"{dayGunHuntCosts.Average():0.00}" : "N/A";
-            Console.WriteLine($"Average cost of day gun hunts: ${avgDayGunHunts}");
-
-            string highestHuntingRate = otherHuntRates.Count > 0 ? $"{otherHuntRates.Max():0.00}" : "N/A";
-            Console.WriteLine($"Highest hunting rate (excluding day 1 hunts with 2+ days): ${highestHuntingRate}");
-
-            //Thank you message.
-            Console.WriteLine("\nThank you for using the Hunting App!");
+            // Print of summary values
+            Console.WriteLine("The number of hunters is " + numberOfHunters + ". ");
+            Console.WriteLine("The total hunting rate for all hunters is " + totalHuntRate + ". ");
+            Console.WriteLine("The average hunting rate for all hunters is " + avgHuntRate + ". ");
+            Console.WriteLine("The number of hunters with shift not equal to 'Day' and number of days >= 2 is " + numberOfHuntersn2 + ". ");
+            Console.WriteLine("The total hunting rate for these hunters is " + totalHuntRaten2 + ". ");
+            Console.WriteLine("The average hunting rate for these hunters is " + avgHuntRaten2 + ". ");
         }
     }
 }
 
-//End of program
